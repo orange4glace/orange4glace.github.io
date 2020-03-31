e48bc22b595f51b22a11535dc1ed8d2ab9e40c72
orange4glace <orange4glace@gmail.com>
2020-03-31 22:17:54 +0900

    update: works

--- works/onepage.js
+++
@@ -1,9 +0,0 @@
-new fullpage('#fullpage', {
-	//options here
-  licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-	autoScrolling:true,
-	scrollHorizontally: true
-});
-
-//methods
-fullpage_api.setAllowScrolling(false);
